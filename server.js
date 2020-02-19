7c6d9e69d7f7b262cc422a9cee22fd44f425ed1e
obaye <hi.savis@gmail.com>
2020-02-19 11:48:14 +0000

    Update package.json - edit name, server.js - edit PORT to 5070

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use('/api/v1/ownbio', require('./routes/api/v1/ownbio'));
 app.use('/api/v1/pets', require('./routes/api/v1/pets'));
 // app.use('/api/v1/todos', require('./routes/api/v1/todos'));
  
-const PORT = process.env.PORT || 5060;
+const PORT = process.env.PORT || 5070;
 
 // app.listen(PORT, () => {
 //   console.log('Server running on port ', PORT)
