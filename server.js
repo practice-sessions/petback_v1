5b59af10763f68c084201daff23501dc3ff95dba
obaye <hi.savis@gmail.com>
2020-03-11 10:59:18 +0000

    Update users route component in v1 api; add commented out password and password2 conditional in register route

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.use('/api/v1/auth', require('./routes/api/v1/auth'));
 app.use('/api/v1/ownbio', require('./routes/api/v1/ownbio'));
 app.use('/api/v1/pets', require('./routes/api/v1/pets'));
  
-const PORT = process.env.PORT || 5070;
+const PORT = process.env.PORT || 5770;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
